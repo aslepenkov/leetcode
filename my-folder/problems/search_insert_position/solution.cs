1e2b18c2d20c421c7402c222c2fd6932a8e00099
aslepenkov <alexmichel249@gmail.com>
2021-11-25 10:10:09 +0000

    Sync LeetCode submission - Search Insert Position (csharp)

---
+++ my-folder/problems/search_insert_position/solution.cs
@@ -0,0 +1,13 @@
+public class Solution {
+            public int SearchInsert(int[] nums, int target)
+            {
+                for (var index = 0; index < nums.Length; index++)
+                {
+                    if (target <= nums[index])
+                    {
+                        return index;
+                    }
+                }
+                return nums.Length;
+            }
+}
