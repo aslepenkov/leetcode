d00cd5480eca3164eeb08f0b3bfadbb7f3b2ebbd
aslepenkov <alexmichel249@gmail.com>
2022-10-03 13:09:47 +0000

    Sync LeetCode submission - Symmetric Tree (csharp)

---
+++ problems/symmetric_tree/solution.cs
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    
+    public bool IsSymmetric(TreeNode root) {
+        return root == null || IsSymm(root.left, root.right);
+    }
+    
+    public bool IsSymm(TreeNode node1, TreeNode node2) {
+        
+      if (node1 == null && node2 == null) 
+          return true;
+      if (node1 == null || node2 == null) 
+          return false;
+      if (node1.val != node2.val) 
+          return false;
+        
+       return IsSymm(node1.left, node2.right) && IsSymm(node1.right, node2.left);
+    }
+}
